dcff16c56bbd2a12d6702e41b76808684207ccad
Faysal_win <faysal.swe27@gmail.com>
2025-03-20 09:49:22 +0600

    Added folders

---
+++ ASP.NetDevSkill/Demo.Web/Demo/WebModule.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using Demo.Models;
+
+namespace Demo
+{
+    public class WebModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<Item>().As<IItem>().SingleInstance();
+            base.Load(builder);
+        }
+    }
+}
