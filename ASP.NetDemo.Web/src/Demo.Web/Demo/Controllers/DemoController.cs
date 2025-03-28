f5ca1016a82a5ba4c672c2a23e5d1cc8e6db5c4e
Faysal_win <faysal.swe27@gmail.com>
2025-03-28 15:30:55 +0600

    Added from in Asp.newDemo project

---
+++ ASP.NetDemo.Web/src/Demo.Web/Demo/Controllers/DemoController.cs
@@ -0,0 +1,20 @@
+﻿using Demo.Models.Demo;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Controllers
+{
+    public class DemoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var model = new IndexModel();
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Index(IndexModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ ASP.NetDemo.Web/src/Demo.Web/Demo/Models/Demo/IndexModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo.Models.Demo
+{
+    public class IndexModel
+    {
+        //public string Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
