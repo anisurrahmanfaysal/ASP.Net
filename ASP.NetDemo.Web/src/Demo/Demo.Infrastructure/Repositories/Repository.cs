754c883ecbd984df928dc570a95f7ac8c9697084
Faysal_win <faysal.swe27@gmail.com>
2025-04-10 09:11:12 +0600

    Added repository pattern

---
+++ ASP.NetDemo.Web/src/Demo/Demo.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,24 @@
+﻿using Demo.Domain.Entities;
+using Demo.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Infrastructure.Repositories
+{
+    public class Repository<T,G> : IRepository<T,G> where T : IEntity<G>
+    {
+        private readonly DbContext _dbContext;
+
+        public Repository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void Add(T entity) { }
+        public void Update(T entity) { }
+        public void Delete(T entity) { }
+    }
+}
